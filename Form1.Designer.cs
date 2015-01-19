00e61f0bcab1736cfeaebb5d037728d6bf682cc6
Wojtek Dudzik <wojtek.adam.dudzik@gmail.com>
2015-01-19 11:06:25 +0100

    Init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace IO_Projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.wypozycz = new System.Windows.Forms.Button();
+            this.zaplac = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(594, 143);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 205);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(594, 150);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // wypozycz
+            // 
+            this.wypozycz.Location = new System.Drawing.Point(531, 393);
+            this.wypozycz.Name = "wypozycz";
+            this.wypozycz.Size = new System.Drawing.Size(75, 23);
+            this.wypozycz.TabIndex = 2;
+            this.wypozycz.Text = "Wypożycz";
+            this.wypozycz.UseVisualStyleBackColor = true;
+            this.wypozycz.Click += new System.EventHandler(this.wypozycz_Click);
+            // 
+            // zaplac
+            // 
+            this.zaplac.Location = new System.Drawing.Point(12, 393);
+            this.zaplac.Name = "zaplac";
+            this.zaplac.Size = new System.Drawing.Size(75, 23);
+            this.zaplac.TabIndex = 3;
+            this.zaplac.Text = "Zapłać";
+            this.zaplac.UseVisualStyleBackColor = true;
+            this.zaplac.Click += new System.EventHandler(this.zaplac_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 450);
+            this.Controls.Add(this.zaplac);
+            this.Controls.Add(this.wypozycz);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button wypozycz;
+        private System.Windows.Forms.Button zaplac;
+    }
+}
+
---
+++ Wypozyczenie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO_Projekt
+{
+    class Wypozyczenie
+    {
+
+        public void wypozycz()
+        {
+            var context = new IO_Projekt.IOEntities();
+            //var query = context
+        }
+
+    }
+}
